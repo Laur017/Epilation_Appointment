8c114027817e19df8187c3aa093b041ad0b08798
Laurentiu Popa <popa.laur.01@gmail.com>
2024-03-08 14:52:33 +0200

    boiler plate for booking

--- client/src/components/Book/Book.tsx
+++ client/src/components/Book/Book.tsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from 'react'
+import './Book.css'
+
 export default function Book() {
+    const [typeBook, setTypeBook] = useState<number>(1);
+
+    const changeTypeOfBook = (n:number):void =>{
+        setTypeBook(n)
+    }
+
+    useEffect(() => {
+        console.log(typeBook)
+    },[typeBook])
+    
   return (
-    <div>Book</div>
+    <div className="book-div">
+        <h2>Book your visit now !</h2>
+        <label>
+            Insert your name 
+            <input type="text" placeholder="eg. John Smith"/>
+        </label>
+        <label>
+            Phone number
+            <input type="text" placeholder="eg. +123456789"/>
+        </label>
+
+        <div className="type-book">
+            <button onClick={() => changeTypeOfBook(1)}>Small</button>
+            <button onClick={() => changeTypeOfBook(2)}>Medium</button>
+            <button onClick={() => changeTypeOfBook(3)}>Big</button>
+        </div>
+
+        <label>
+            Date
+            <input type="date" placeholder="eg. +123456789"/>
+        </label>
+        <label>
+            Hour
+            <input type="time" placeholder="eg. +123456789"/>
+        </label>
+
+        <button className='book-btn'>Book now</button>
+    </div>
   )
 }
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,12 +1,14 @@
 import MainImage from '../assets/home-img.jpg'
+import { useNavigate } from 'react-router-dom'
 
 export default function Home() {
+  const navigate = useNavigate()
   return (
     <div className="home-div">
         <div>
             <h2>The name and something more</h2>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur voluptatem itaque veritatis delectus minima alias reiciendis consequatur obcaecati natus in dignissimos eligendi ut officia animi quis, porro omnis atque recusandae.</p>
-            <button>Book now !</button>
+            <button onClick={() => navigate('/book')}>Book now !</button>
         </div>
         <img src={MainImage} />
     </div>
