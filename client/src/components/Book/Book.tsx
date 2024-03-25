a93c923615bad1dce8dfce9a163c73b488aa5143
Laurentiu Popa <popa.laur.01@gmail.com>
2024-03-25 17:34:21 +0200

    updated date and time picker

--- client/src/components/Book/Book.tsx
+++ client/src/components/Book/Book.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react'
 import './Book.css'
+import Arm from '../../assets/elbow.png'
+import Armpit from '../../assets/armpit.png'
+import Leg from '../../assets/leg.png'
 
 export default function Book() {
     const [typeBook, setTypeBook] = useState<number>(1);
@@ -11,35 +14,55 @@ export default function Book() {
     useEffect(() => {
         console.log(typeBook)
     },[typeBook])
-    
+
+    const azi = new Date()
+    azi.setDate(azi.getDate() + 1)
+    const day = azi.getDate()
+    const month = azi.getMonth() + 1
+    const year = azi.getFullYear()
+    const aziString = `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`
+
   return (
     <div className="book-div">
-        <h2>Book your visit now !</h2>
-        <label>
-            Insert your name 
-            <input type="text" placeholder="eg. John Smith"/>
-        </label>
-        <label>
-            Phone number
-            <input type="text" placeholder="eg. +123456789"/>
-        </label>
+        <h2>Programează o rezervare acum !</h2>
+        <div className="book-content">
 
+        <label>
+            Nume și Prenume *
+            <input type="text" placeholder="eg. John Smith" required/>
+        </label>
+        <label>
+            Număr de telefon *
+            <input type="text" placeholder="eg. 069123456" required/>
+        </label>
+        <label>Tipul procedurii *</label>
         <div className="type-book">
-            <button onClick={() => changeTypeOfBook(1)}>Small</button>
-            <button onClick={() => changeTypeOfBook(2)}>Medium</button>
-            <button onClick={() => changeTypeOfBook(3)}>Big</button>
+            <button onClick={() => changeTypeOfBook(1)} className={typeBook === 1 ? "selected-type" : ""}><img src={Arm} /></button>
+            <button onClick={() => changeTypeOfBook(2)} className={typeBook === 2 ? "selected-type" : ""}><img src={Armpit} /></button>
+            <button onClick={() => changeTypeOfBook(3)} className={typeBook === 3 ? "selected-type" : ""}><img src={Leg} /></button>
         </div>
 
         <label>
-            Date
-            <input type="date" placeholder="eg. +123456789"/>
+            Data *
+            <input type="date" min={aziString} value={aziString}/>
         </label>
         <label>
-            Hour
-            <input type="time" placeholder="eg. +123456789"/>
+            Ora *
+            <select className='select-time'>
+                <option value={"09"}>09:00</option>
+                <option value={"10"}>10:00</option>
+                <option value={"11"}>11:00</option>
+                <option value={"12"}>12:00</option>
+                <option value={"13"}>13:00</option>
+                <option value={"14"}>14:00</option>
+                <option value={"15"}>15:00</option>
+                <option value={"16"}>16:00</option>
+                <option value={"17"}>17:00</option>
+            </select>
         </label>
 
-        <button className='book-btn'>Book now</button>
+        </div>
+        <button className='book-btn'>Rezervă</button>
     </div>
   )
 }
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,22 @@
-import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useNavigate, useLocation} from "react-router-dom"
 
 export default function Navbar() {
-    const navigate = useNavigate();
+    const navigate = useNavigate()
+    const location = useLocation()
+    const [landingHere, setLandingHere] = useState<boolean>(true)
 
     const goTo = (path:string):void => {
+        path !== '/' ? setLandingHere(false) : setLandingHere(true)
         navigate(path)
     }
+
+    useEffect(() => {
+      location.pathname !== '/' && setLandingHere(false)
+    },[location])
+
   return (
-    <nav>
+    <nav  className={`${landingHere ? "" : "nav-dark"}`}>
         <h1  onClick={() => goTo('/')}>ElectroGlow</h1>
 
         <div>
