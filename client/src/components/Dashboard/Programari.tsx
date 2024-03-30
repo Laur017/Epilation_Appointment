fb8971dee0911a159f80069a5cf5a44a15103bd6
Laurentiu Popa <popa.laur.01@gmail.com>
2024-03-30 14:45:44 +0200

    Update Programari.tsx

--- client/src/components/Dashboard/Programari.tsx
+++ client/src/components/Dashboard/Programari.tsx
@@ -1,8 +1,8 @@
-import {Calendar, momentLocalizer} from 'react-big-calendar'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 import { useEffect, useState } from 'react';
 import { doc, getDoc } from 'firebase/firestore'
-import {db} from '../../firebase';
+import { db } from '../../firebase';
 
 const localizer = momentLocalizer(moment)
 
@@ -31,53 +31,53 @@ interface Event {
   start: Date;
   end: Date;
   title: string;
-} 
+}
 
 export default function Programari() {
   const [events, setEvents] = useState<Event[]>([])
   const today = new Date();
   const startOfDay = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 9, 0);
   const endOfDay = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 18, 0);
-  
-  useEffect(() => {
-      const fetchEvents = async () => {
-        try {
-          const docRef = doc(db, "electro", 'programari');
-          const docSnap = await getDoc(docRef);
-          
-          if (docSnap.exists()) {
-            const data = docSnap.data();
-            console.log(data)
 
-            const fetchedEvents =  Object.values(data).map((i) => ({
-              start:moment(`${i.data.split('-').reverse().join('-')}T${i.ora}:00`).toDate(),
-              end:moment(`${i.data.split('-').reverse().join('-')}T${(parseInt(i.ora)+1).toString()}:00`).toDate(),
-              title:`${i.nume} - ${i.procedura} - ${i.numar}`
-            }))
-            setEvents(fetchedEvents)
-          }else {
-            console.log("No such document exists");
-          }
-        }catch (error){
-          console.error(error)
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const docRef = doc(db, "electro", 'programari');
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          console.log(data)
+
+          const fetchedEvents = Object.values(data).map((i) => ({
+            start: moment(`${i.data.split('-').reverse().join('-')}T${i.ora}:00`).toDate(),
+            end: moment(`${i.data.split('-').reverse().join('-')}T${(parseInt(i.ora) + 1).toString()}:00`).toDate(),
+            title: `${i.nume} - ${i.procedura} - ${i.numar}`
+          }))
+          setEvents(fetchedEvents)
+        } else {
+          console.log("No such document exists");
         }
-      } 
-      fetchEvents()
-  },[])
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchEvents()
+  }, [])
 
   useEffect(() => {
 
     console.log(events)
 
-  },[events])
+  }, [events])
   return (
     <div className="programari-div">
       <div className="calendar-div">
-       <Calendar 
-        events={events} 
-        localizer={localizer}
-        min={startOfDay}
-        max={endOfDay}
+        <Calendar
+          events={events}
+          localizer={localizer}
+          min={startOfDay}
+          max={endOfDay}
         />
       </div>
     </div>
